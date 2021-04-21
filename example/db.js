7b919582a6b8f6f987537a7a8996bda6cff0ffc3
davidvaughan86 <david.vaughan86@gmail.com>
2021-04-21 16:43:26 -0400

    databases

--- example/db.js
+++ example/db.js
@@ -1,9 +1,12 @@
 const Pool = require('pg').Pool
+//or
+// const { Pool } = require('pg')
+
 // pg will allow us to connect an isntance of our database ( a pool class/instance/object)
 // allows our server to send requests to it
 const pool = new Pool ({
     user: "king",
-    password: "311Stars",
+    password: "",
     host: "localhost",
     port: 5432,
     database:"todolist"
